90b544cecc8d3403456c32e592f4c5f65f2ac925
Marina <marina52746@yandex.ru>
2023-09-10 17:10:46 +0300

    fixes

--- server/src/main/resources/testRequestData.sql
+++
@@ -1,5 +0,0 @@
-MERGE INTO requests (id, description, requestor_id, created) VALUES
-(1, 'нужна кинокамера', 5, CURRENT_TIMESTAMP),
-(2, 'одолжу резиновую лодку на 2 человек на неделю', 1, CURRENT_TIMESTAMP),
-(3, '2 раскладушки', 2, CURRENT_TIMESTAMP),
-(4, 'палатка', 5, CURRENT_TIMESTAMP);
--- server/src/main/resources/testUserData.sql
+++
@@ -1,6 +0,0 @@
-MERGE INTO users (id, name, email) VALUES
-(1, 'alex', 'alex@mail.ru'),
-(2, 'elena', 'elena@mail.ru'),
-(3, 'olga', 'olga@mail.ru'),
-(4, 'sergey', 'sergey@mail.ru'),
-(5, 'misha', 'misha@mail.ru');
