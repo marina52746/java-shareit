9fef725a2f621e5ac7a0f148f5a40389df448ef7
Marina <marina52746@yandex.ru>
2023-09-11 19:50:11 +0300

    add line

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
 import ru.practicum.shareit.booking.dto.InputBookingDto;
 import ru.practicum.shareit.booking.dto.State;
 import ru.practicum.shareit.exception.ValidationException;
